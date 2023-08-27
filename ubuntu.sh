bae8346e444b9c9a9c68017c893d5f526bf232ad
Andrzej Barabasz <abarabasz@gmail.com>
2023-08-27 13:05:25 +0200

    update

---
+++ ubuntu.sh
@@ -0,0 +1,60 @@
+#!/bin/zsh
+
+# Update, upgrade & cleanup
+sudo apt update && sudo apt -y upgrade
+sudo apt autoremove
+
+# Git & GitHub
+sudo apt -y install git gh
+gh auth login
+
+# Clone bin
+if [ ! -d "~/GitHub/bin" ]; then
+    gh repo clone barabasz/bin
+fi
+ln -sf ~/GitHub/bin/linux ~/bin
+
+# Clone bintest
+if [ ! -d "~/GitHub/bintest" ]; then
+    gh repo clone barabasz/bintest
+fi
+ln -sf ~/GitHub/bintest/linux ~/bintest
+
+# Clone config
+mkdir -p ~/GitHub && cd $_
+if [ ! -d "~/GitHub/config" ]; then
+    gh repo clone barabasz/config
+fi
+
+# Locale
+sudo apt install language-pack-pl-base language-pack-pl
+sudo locale-gen pl_PL && sudo locale-gen pl_PL.UTF-8
+sudo cp -f ~/GitHub/config/default/locale /etc/default/locale
+
+# zsh & omz
+ln -sf ~/GitHub/config/zsh/.zshrc ~/.zshrc
+ln -sf ~/GitHub/config/zsh/.zprofile ~/.zprofile
+ln -sf ~/GitHub/config/zsh/.p10k.zsh ~/.p10k.zsh
+sudo apt install zsh
+chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Certs & security
+sudo apt install -y openssh-server gnupg2 ca-certificates acl
+
+# Basic tools
+sudo apt install -y curl net-tools lsb-release apt-transport-https software-properties-common
+
+# mc (Midnight Commander)
+sudo apt install -y mc
+mkdir -p ~/.config/mc
+ln -sf ~/GitHub/config/mc/ini ~/.config/mc/ini
+ln -sf ~/GitHub/config/mc/panels.ini ~/.config/mc/panels.ini
+
+# nvim
+
+# Brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
---
+++ zsh/.zshrc
@@ -0,0 +1,56 @@
+export ZSH_COMPDUMP=$ZSH/cache/.zcompdump-$HOST
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
+# INCLUDES
+[ -f ~/.bash_profile ] && source ~/.bash_profile
+[ -f ~/.iterm2_shell_integration.zsh ] && source ~/.iterm2_shell_integration.zsh
+
+export PATH=$HOME/bin:/usr/local/bin:$PATH
+export ZSH="$HOME/.oh-my-zsh"
+ZSH_THEME="powerlevel10k/powerlevel10k"
+
+zstyle ':omz:update' mode reminder
+zstyle ':omz:update' frequency 1
+
+# DISABLE_MAGIC_FUNCTIONS="true"
+# DISABLE_LS_COLORS="true"
+# DISABLE_AUTO_TITLE="true"
+# ENABLE_CORRECTION="true"
+COMPLETION_WAITING_DOTS="true"
+# DISABLE_UNTRACKED_FILES_DIRTY="true"
+
+plugins=(copyfile copypath history sudo web-search zsh-autosuggestions zsh-syntax-highlighting macos)
+source $ZSH/oh-my-zsh.sh
+
+# User configuration
+
+export PS1="[%{%F{cyan}%}%n%{%f%}@%{%F{green}%}%m:%{%F{yellow}%}%~%{%f%}%]]$ "
+
+# export MANPATH="/usr/local/man:$MANPATH"
+
+# You may need to manually set your language environment
+export LANG=en_US.UTF-8
+
+# Preferred editor for local and remote sessions
+if [[ -n $SSH_CONNECTION ]]; then
+  export EDITOR='nvim'
+else
+ export EDITOR='nvim'
+fi
+
+# Aliases
+alias vim="nvim"
+alias vi="nvim"
+alias view="nvim -R"
+alias vimdiff="nvim -d"
+alias mc="mc --nosubshell"
+alias myip="curl icanhazip.com" # Show public IP
+alias python=python3
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
