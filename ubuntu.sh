f5712d7751583502d6cfa651e35ce5b9c4b04b8b
Andrzej Barabasz <abarabasz@gmail.com>
2023-08-27 19:38:41 +0200

    ext install files

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,67 +1,33 @@
 #!/bin/bash
 
-export NEEDRESTART_MODE=a
-
-# Update, upgrade & cleanup
-sudo NEEDRESTART_MODE=a apt update && sudo apt -y upgrade
-sudo apt autoremove
-
-# Certs & security
-sudo NEEDRESTART_MODE=a apt install -y openssh-server gnupg2 ca-certificates acl
-
-# Basic tools
-sudo NEEDRESTART_MODE=a apt install -y net-tools lsb-release apt-transport-https software-properties-common
-
-# Git & GitHub
+# Git, gh & repos
 sudo NEEDRESTART_MODE=a apt -y install git gh
 gh auth login
 mkdir -p ~/GitHub && cd $_
-
-# Clone bin
-if [ ! -d "~/GitHub/bin" ]; then
-    gh repo clone barabasz/bin
-fi
+gh repo clone barabasz/bin
 ln -sf ~/GitHub/bin/linux ~/bin
-
-# Clone bintest
-if [ ! -d "~/GitHub/bintest" ]; then
-    gh repo clone barabasz/bintest
-fi
+gh repo clone barabasz/bintest
 ln -sf ~/GitHub/bintest/linux ~/bintest
-
-# Clone config
-if [ ! -d "~/GitHub/config" ]; then
-    gh repo clone barabasz/config
-fi
-
-# Disable login info
-sudo chmod -x /etc/update-motd.d/10-help-text
-sudo chmod -x /etc/update-motd.d/50-motd-news
-sudo chmod -x /etc/update-motd.d/91-contract-ua-esm-status
-
-# Set locale
-sudo apt install language-pack-pl-base language-pack-pl
-sudo locale-gen pl_PL && sudo locale-gen pl_PL.UTF-8
-sudo cp -f ~/GitHub/config/default/locale /etc/default/locale
+gh repo clone barabasz/config
 
 # zsh & omz
-ln -sf ~/GitHub/config/zsh/.zshrc ~/
-ln -sf ~/GitHub/config/zsh/.zprofile ~/
-ln -sf ~/GitHub/config/zsh/.p10k.zsh ~/
-sudo apt install zsh
-chsh -s $(which zsh)
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+install-zsh
+install-omz
+
+# Core tools & apps
+install-essentials
+
+# Remove snap
+remove-snap
+
+# Minimize login info
+fix-login-info
+
+# Set locale
+set-locale
 
 # mc (Midnight Commander)
-sudo apt install -y mc
-mkdir -p ~/.config/mc
-ln -sf ~/GitHub/config/mc/ini ~/.config/mc/ini
-ln -sf ~/GitHub/config/mc/panels.ini ~/.config/mc/panels.ini
+install-mc
 
-# nvim
-
-# Brew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# nvim (neovim)
+install-nvim
