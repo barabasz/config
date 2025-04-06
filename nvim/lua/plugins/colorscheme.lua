5b0dda0fafdc30db8178813ab2c44f244f196f25
Andrzej Barabasz <abarabasz@gmail.com>
2025-04-06 14:51:13 +0200

    update

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,13 @@
 -- Tokyo Night
 -- A dark and light Neovim theme written in Lua ported from the Visual Studio Code TokyoNight theme.
+local theme = "night" -- The style of the theme, can be one of: storm, moon, day, night
 return {
     "folke/tokyonight.nvim",
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
-    opts = {},
+    opts = { style = theme },
     config = function()
-        vim.cmd("colorscheme tokyonight-night") -- nigh, storm, day, moon
+        vim.cmd("colorscheme tokyonight-" .. theme)
     end,
 }
  
