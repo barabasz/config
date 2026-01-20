26fd1a64f1b721a01d27e9b4bf53f3f0be7daab7
Andrzej Barabasz <abarabasz@gmail.com>
2026-01-20 23:49:46 +0100

    execs fis

--- zsh/inc/variables.zsh
+++ zsh/inc/variables.zsh
@@ -12,5 +12,9 @@ export LOG_SHOW_ICONS=1  # log.sh: 1 for icons, 0 for nothing
 export LOG_COLOR_TEXTS=1 # log.sh: 1 for colors, 0 for white
 export LOG_EMOJI_ICONS=0 # log.sh: 1 for emoji, 0 for text
 
+# Execs
+export EXECS_SPINNER_FRAMES=('⣷' '⣯' '⣟' '⡿' '⢿' '⣻' '⣽' '⣾')
+export EXECS_SPINNER_DELAY=0.10
+
 # shell files tracking - keep at the end
 zfile_track_end ${0:A}
